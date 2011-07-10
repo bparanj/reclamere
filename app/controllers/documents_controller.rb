f63681f58a83ac10d27a74a91c476910d07dd42f
Bala Paranj <bparanj@yahoo.com>
2011-07-09 19:07:26 -0700

    Before implementing internal documents tab

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -4,83 +4,52 @@ class DocumentsController < ApplicationController
   before_filter :get_document, :except => [ :new, :create ]
   helper :folders
 
-  # GET /documents/1
-  # GET /documents/1.xml
   def show
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @document }
-    end
   end
 
-  # GET /documents/new
-  # GET /documents/new.xml
   def new
     @document = @folder.documents.new
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @document }
-    end
   end
 
-  # GET /documents/1/edit
   def edit
   end
 
-  # POST /documents
-  # POST /documents.xml
   def create
     @document = Document.new(params[:document].merge({ :created_by => current_user }))
     @document.folder = @folder unless @document.folder
-    respond_to do |format|
-      if @document.save
-        donemessage 'Success!', "Document \"#{@document.name}\" was successfully created."
-        audit "Created document \"#{@document.name}\"", :auditable => @folderable
-        format.html { redirect_to polymorphic_path([@folderable, @document.folder]) }
-        format.xml  { render :xml => @document, :status => :created, :location => @document }
-      else
-        format.html do
-          render :action => "new"
-        end
-        format.xml  { render :xml => @document.errors, :status => :unprocessable_entity }
-      end
+    
+    if @document.save
+      donemessage 'Success!', "Document \"#{@document.name}\" was successfully created."
+      audit "Created document \"#{@document.name}\"", :auditable => @folderable
+      redirect_to polymorphic_path([@folderable, @document.folder])
+    else
+      render :action => "new"
     end
   end
 
-  # PUT /documents/1
-  # PUT /documents/1.xml
   def update
-    respond_to do |format|
-      @document.attributes = params[:document]
-      if @document.create_new_version?
-        @document.created_by = current_user
-      end
-      @document.folder = @folder unless @document.folder
-      if @document.save
-        donemark "Document \"#{@document.name}\" was successfully updated."
-        audit "Updated document \"#{@document.name}\"", :auditable => @folderable
-        format.html { redirect_to polymorphic_path([@folderable, @document.folder, @document]) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @document.errors, :status => :unprocessable_entity }
-      end
+    @document.attributes = params[:document]
+    if @document.create_new_version?
+      @document.created_by = current_user
+    end
+    @document.folder = @folder unless @document.folder
+    if @document.save
+      donemark "Document \"#{@document.name}\" was successfully updated."
+      audit "Updated document \"#{@document.name}\"", :auditable => @folderable
+      redirect_to polymorphic_path([@folderable, @document.folder, @document])
+    else
+      render :action => "edit"
     end
   end
 
-  # DELETE /documents/1
-  # DELETE /documents/1.xml
   def destroy
     @document.destroy
     donemark "Document \"#{@document.name}\" was deleted successfully."
     audit "Deleted document \"#{@document.name}\"", :auditable => @folderable
-    respond_to do |format|
-      format.html { redirect_to polymorphic_path([@folderable, @document.folder]) }
-      format.xml  { head :ok }
-    end
+    
+    redirect_to polymorphic_path([@folderable, @document.folder])    
   end
 
-  # download document
   def download
     if params[:version]
       document = @document.versions.find(params[:version])
@@ -99,7 +68,6 @@ class DocumentsController < ApplicationController
     end
   end
 
-  # show document versions
   def versions
     @versions = @document.versions.find(:all, :order => 'version DESC')
   end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,15 @@
 class SearchController < ApplicationController
+  
   def new
   end
   
-  def index
-    @pickups = Pickup.find(:all, 
-      :conditions => ['lot_number = ? or  client_reference = ?', params[:lot_number], params[:client_reference]]) 
+  def index    
+    if client_user?
+      cond = ['(lot_number = ? or  client_reference = ?) and client_id = ?', params[:lot_number], params[:client_reference], current_user.client_id]
+    else
+      cond = ['lot_number = ? or  client_reference = ?', params[:lot_number], params[:client_reference]]
+    end
+    @pickups = Pickup.find(:all, :conditions => cond) 
   end
 
 end
